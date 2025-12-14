3c25f933b1a949f4103cf821dc488de7008fbc35
Adcage <2042234720@qq.com>
2025-12-15 00:17:28 +0800

    feat: 更新路由配置和主布局

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import { TitleBar } from '../components/TitleBar'
-import { ThemeSwitch } from '../components/ThemeSwitch'
-import LanguageSwitch from '../components/LanguageSwitch'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import { TitleBar } from '../components/TitleBar';
+import { ThemeSwitch } from '../components/ThemeSwitch';
+import LanguageSwitch from '../components/LanguageSwitch';
 
 const MainLayout: React.FC = () => {
   return (
-    <div className='flex h-screen flex-col overflow-hidden bg-gray-50 text-gray-900 dark:bg-zinc-950 dark:text-gray-100'>
+    <div className="h-screen flex flex-col bg-gray-50 dark:bg-zinc-950 text-gray-900 dark:text-gray-100 overflow-hidden">
       <TitleBar />
-      <div className='fixed right-4 top-14 z-50'>
+      <div className="fixed top-14 right-4 z-50">
         <ThemeSwitch />
       </div>
-      <div className='fixed right-20 top-14 z-50'>
+      <div className="fixed top-14 right-20 z-50">
         <LanguageSwitch />
       </div>
-      <main className='flex-1 overflow-auto p-8'>
+      <main className="flex-1 overflow-auto p-8">
         <Outlet />
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default MainLayout
+export default MainLayout;
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,12 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import MainLayout from '../layouts/MainLayout'
-import HomePage from '../pages/HomePage'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainLayout from '../layouts/MainLayout';
+import HomePage from '../pages/HomePage';
+import DashboardPage from '../pages/DashboardPage';
+import ServersPage from '../pages/ServersPage';
+import SyncFoldersPage from '../pages/SyncFoldersPage';
+import LogsPage from '../pages/LogsPage';
+import SettingsPage from '../pages/SettingsPage';
+import AboutPage from '../pages/AboutPage';
 
 const router = createBrowserRouter([
   {
@@ -11,8 +17,32 @@ const router = createBrowserRouter([
         index: true,
         element: <HomePage />,
       },
+      {
+        path: 'dashboard',
+        element: <DashboardPage />,
+      },
+      {
+        path: 'servers',
+        element: <ServersPage />,
+      },
+      {
+        path: 'folders',
+        element: <SyncFoldersPage />,
+      },
+      {
+        path: 'logs',
+        element: <LogsPage />,
+      },
+      {
+        path: 'settings',
+        element: <SettingsPage />,
+      },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
     ],
   },
-])
+]);
 
-export const AppRouter = () => <RouterProvider router={router} />
+export const AppRouter = () => <RouterProvider router={router} />;
